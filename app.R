1ff83573c442240002fa118747e5e2288251b7f5
hsirish98 <hannahsirish@gmail.com>
2023-03-08 12:22:09 -0800

    fixed totaling second tab and added labels

--- app.R
+++ app.R
@@ -18,13 +18,13 @@ ui <- fluidPage(
   theme=my_theme,
   navbarPage("Energy Modeling",
              tabPanel("Overview",
-                        mainPanel(strong(p("Welcome to my shiny app!")),
-                                    p("The purpose of this shiny app is to take a look at current
+                      mainPanel(strong(p("Welcome to my shiny app!")),
+                                p("The purpose of this shiny app is to take a look at current
                                       trends in residential/personal energy consumption, and 
                                       get a feel for the task it will take to electrify the residential
                                       sector")
-                      
-                        )
+                                
+                      )
              ),
              tabPanel("Dashboard: Total Residential Energy Consumption (1997-2020)",
                       sidebarLayout(
@@ -58,13 +58,13 @@ ui <- fluidPage(
                                      # end radioButtons
                         ), # end sidebarPanel
                         
-                  
+                        
                         mainPanel(
                           tabsetPanel(
                             tabPanel("Overview", plotOutput("fe_plot")),
                             tabPanel("Total by Region",plotOutput("fe_totals"))
                           )
-  
+                          
                         )
                         
                       )
@@ -74,7 +74,7 @@ ui <- fluidPage(
              tabPanel("Electrification of the States",
                       sidebarLayout(
                         sidebarPanel("Choose the Housing Characteristic:",
-                              
+                                     
                                      switchInput(
                                        inputId = "pick_mode",
                                        label = NULL,
@@ -90,10 +90,10 @@ ui <- fluidPage(
                                        inline = FALSE,
                                        width = NULL
                                      )
-                                                        
-                                     ),
-                                  
-                      
+                                     
+                        ),
+                        
+                        
                         mainPanel(
                           tabsetPanel(
                             tabPanel("Analysis",textOutput("elec_summary")),
@@ -109,14 +109,14 @@ ui <- fluidPage(
              tabPanel("Energy Insecurity",
                       sidebarLayout(
                         sidebarPanel(
-                                     checkboxGroupInput(inputId = "pick_appliance",
-                                                        label = "Choose Appliance(s):",
-                                                        choices = c("Space Heating", "Air Conditioning",
-                                                                    "Water Heating", "Clothes Wasing",
-                                                                    "Clothes Drying", "Lighting", "Refrigeration",
-                                                                    "Cooking", "Dishwasher", "TVs", "Pool Pumps", 
-                                                                    "Hot Tub Pumps and Heaters"
-                                                        ))
+                          checkboxGroupInput(inputId = "pick_appliance",
+                                             label = "Choose Appliance(s):",
+                                             choices = c("Space Heating", "Air Conditioning",
+                                                         "Water Heating", "Clothes Wasing",
+                                                         "Clothes Drying", "Lighting", "Refrigeration",
+                                                         "Cooking", "Dishwasher", "TVs", "Pool Pumps", 
+                                                         "Hot Tub Pumps and Heaters"
+                                             ))
                         ), # end checkboxGroupInput
                         
                         # end sidebarPanel
@@ -133,40 +133,47 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-## Tab 1: Overview
-
-
+  ## Tab 1: Overview
   
-## Tab 3: Region + End Use + Fuel
+  
+  
+  ## Tab 3: Region + End Use + Fuel
   ## Taking Inputs
   fe_reactive <- reactive({
     ## if "ALL" is selected
     if(input$pick_place=="All"){
-      fuel_use_tot %>%
-        filter(end_use %in% input$pick_use)
+      fuel_use_tidy_test <- fuel_use_tidy %>%
+        filter(end_use %in% input$pick_use) %>%
+        group_by(census_region, fuel) %>%
+        summarize(MJ = sum(MJ, na.rm=TRUE))
+      
     } else{
-    
-    ## if a census region is selected
-    fuel_use_tidy %>%
-      filter(census_region %in% input$pick_place)%>%
-      filter(end_use %in% input$pick_use)
+      
+      ## if a census region is selected
+      fuel_use_tidy_test <- fuel_use_tidy %>%
+        filter(end_use %in% input$pick_use, census_region %in% input$pick_place) %>%
+        group_by(sub_region, fuel) %>%
+        summarize(MJ = sum(MJ, na.rm=TRUE))
+      
     }
   })
   
-
-   ##input for bottom graph
+  
+  ##input for bottom graph
   fe_reactive_tot <- reactive({
     ## if "ALL is selected"
     if(input$pick_place=="All"){
-      fuel_use_coll_all %>%
-        filter(end_use %in% input$pick_use) 
+      fuel_use_tidy %>%
+        filter(end_use %in% input$pick_use) %>%
+        group_by(census_region) %>%
+        summarize(MJ = sum(MJ, na.rm=TRUE))
+      
     } else{
       ## if census region is selected
-      fuel_use_tot %>%
-        filter(census_region %in% input$pick_place)%>%
-        filter(end_use %in% input$pick_use) %>%
-        group_by(sub_region)%>%
-        summarize(sum=sum(btu))
+      fuel_use_tidy %>%
+        filter(end_use %in% input$pick_use, census_region %in% input$pick_place) %>%
+        group_by(fuel) %>%
+        summarize(MJ = sum(MJ, na.rm=TRUE))
     }
   })
   
@@ -176,11 +183,12 @@ server <- function(input, output) {
   output$fe_plot <- renderPlot( {
     ##if "ALL" is selected
     if(input$pick_place=="All"){
-      ggplot(data = fe_reactive(), aes(x=fuel, y=btu, fill=census_region))+
+      ggplot(data = fe_reactive(), aes(x=fuel, y=MJ, fill=census_region))+
         geom_col(position="dodge")+
         scale_fill_manual(values=c("darkslategray", "darkslategray4", "darkslategray3", "slategray"))+
-        labs(x=input$pick_use, y="Fuel (Trillion Btu)", title=input$pick_place, fill="Census Region") +
-        ylim(0,600)+
+        labs(x=input$pick_use, y="Fuel (Million MJ)", title=input$pick_place, fill="Census Region") +
+        ylim(0,15000)+
+        geom_text(aes(label=MJ),position = position_dodge(width = 1))+
         theme_minimal() +
         theme(axis.text.y = element_text(size=12, 
                                          color="black"),
@@ -193,38 +201,13 @@ server <- function(input, output) {
                                           color="darkslategray",
                                           face="bold"))
     } else{
-    ## if census region is selected
-    ggplot(data = fe_reactive(), aes(x=fuel, y=btu, fill=sub_region))+
-      geom_col(position="dodge")+
-      scale_fill_manual(values=c("darkslategray", "darkslategray4", "darkslategray3", "slategray"))+
-      labs(x=input$pick_use, y="Fuel (Trillion Btu)", title=input$pick_place, fill="Sub Region") +
-      ylim(0,600)+
-      theme_minimal() +
-        theme(axis.text.y = element_text(size=12, 
-                                         color="black"),
-              axis.text.x = element_text(size=12, 
-                                         color="black"),
-              axis.title.x = element_text(size=16, 
-                                          color="darkslategray",
-                                          face="bold"),
-              axis.title.y = element_text(size=16, 
-                                          color="darkslategray",
-                                          face="bold"))
-
-    }
-  }, bg="transparent"
-  )
-  
- ##bottom graph (totals)
-  output$fe_totals <- renderPlot( 
-    {
-    ##if census region is selected
-    if(input$pick_place!="All"){
-      ggplot(data = fe_reactive_tot(), aes(x=sub_region, y=sum, fill=sub_region))+
-        geom_col()+
-        scale_fill_manual(values=c("darkgreen", "seagreen3", "seagreen1", "darkseagreen2"))+
-        labs(x=input$pick_use, fill="Sub Region",y="Fuel (Trillion Btu)", title="Total Btu Used by Each Subregion (Trillions") +
-        ylim(0,1000)+
+      ## if census region is selected
+      ggplot(data = fe_reactive(), aes(x=fuel, y=MJ, fill=sub_region))+
+        geom_col(position="dodge")+
+        scale_fill_manual(values=c("darkslategray", "darkslategray4", "darkslategray3", "slategray"))+
+        labs(x=input$pick_use, y="Fuel (Million MJ)", title=input$pick_place, fill="Sub Region") +
+        ylim(0,10000)+
+        geom_text(aes(label=MJ),position = position_dodge(width = 1))+
         theme_minimal() +
         theme(axis.text.y = element_text(size=12, 
                                          color="black"),
@@ -236,40 +219,68 @@ server <- function(input, output) {
               axis.title.y = element_text(size=16, 
                                           color="darkslategray",
                                           face="bold"))
-    } 
-    else{
-      ##if "ALL" is selected
-      ggplot(data = fe_reactive_tot(), aes(x=census_region, y=btu, fill=census_region))+
-        geom_col()+
-        scale_fill_manual(values=c("darkgreen", "seagreen3", "seagreen1", "darkseagreen2"))+
-        labs(x=input$pick_use, fill= "Census Region",y="Fuel (Trillion Btu)", title="Total Btu Used by Each Region (Trillions)") +
-        ylim(0,1000)+
-        theme_minimal()+
-        theme(axis.text.y = element_text(size=12, 
-                                          color="black"),
-              axis.text.x = element_text(size=12, 
-                                          color="black"),
-              axis.title.x = element_text(size=16, 
-                                         color="darkslategray",
-                                         face="bold"),
-              axis.title.y = element_text(size=16, 
-                                          color="darkslategray",
-                                          face="bold")
-              
-              ) 
+      
     }
+  }, bg="transparent"
+  )
+  
+  ##bottom graph (totals)
+  output$fe_totals <- renderPlot( 
+    {
+      ##if census region is selected
+      if(input$pick_place!="All"){
+        ggplot(data = fe_reactive_tot(), aes(x=fuel, y=MJ, fill=fuel))+
+          geom_col()+
+          scale_fill_manual(values=c("darkgreen", "seagreen3", "seagreen1", "darkseagreen2"))+
+          labs(x=input$pick_use, fill="Sub Region",y="Fuel (Million MJ)", title="Total Billion MJ of Each Fuel Used") +
+          ylim(0,15000)+
+          geom_text(aes(label=MJ))+
+          theme_minimal() +
+          theme(axis.text.y = element_text(size=12, 
+                                           color="black"),
+                axis.text.x = element_text(size=12, 
+                                           color="black"),
+                axis.title.x = element_text(size=16, 
+                                            color="darkslategray",
+                                            face="bold"),
+                axis.title.y = element_text(size=16, 
+                                            color="darkslategray",
+                                            face="bold"))
+      } 
+      else{
+        ##if "ALL" is selected
+        ggplot(data = fe_reactive_tot(), aes(x=census_region, y=MJ, fill=census_region))+
+          geom_col()+
+          scale_fill_manual(values=c("darkgreen", "seagreen3", "seagreen1", "darkseagreen2"))+
+          labs(x=input$pick_use, fill= "Census Region",y="Fuel (Million MJ)", title="Total Billion MJ Used by Each Region (All Fuel Types)") +
+          ylim(0,20000)+
+          geom_text(aes(label=MJ))+
+          theme_minimal()+
+          theme(axis.text.y = element_text(size=12, 
+                                           color="black"),
+                axis.text.x = element_text(size=12, 
+                                           color="black"),
+                axis.title.x = element_text(size=16, 
+                                            color="darkslategray",
+                                            face="bold"),
+                axis.title.y = element_text(size=16, 
+                                            color="darkslategray",
+                                            face="bold")
+                
+          ) 
+      }
     }, bg="transparent"
     
   )
   
   
   pct_e_reactive <- reactive({
-   
+    
     
     states_contig_sf 
     
   })
-
+  
   
   output$pct_state <- renderPlot({
     if(input$pick_mode=="TRUE") {
@@ -280,12 +291,12 @@ server <- function(input, output) {
         labs(title = "Percent of Homes Fully Electrified")+
         theme_void() 
     } else(
-    ggplot()+
-      geom_sf(data=pct_e_reactive(), size=0.2,color="black", aes(fill=pct_i))+
-      scale_fill_gradient(low="pink", high="darkred")+
-      labs(fill="Percent of Homes in State")+
-      labs(title = "Percent of Homes Using Natural Gas for Any Reason")+
-      theme_void() 
+      ggplot()+
+        geom_sf(data=pct_e_reactive(), size=0.2,color="black", aes(fill=pct_i))+
+        scale_fill_gradient(low="pink", high="darkred")+
+        labs(fill="Percent of Homes in State")+
+        labs(title = "Percent of Homes Using Natural Gas for Any Reason")+
+        theme_void() 
     )
   }, bg= "transparent"
   
@@ -301,21 +312,12 @@ server <- function(input, output) {
     gas and other fuels to provide much needed services like space heating.
     77% is the max percent of homes electrified in any given state, in Colorado."
   })
-
-
-
+  
+  
+  
 }
 
 shinyApp(ui = ui, server = server)
-
-
-
-
-
-
-
-
-
 
 
 
--- wrangle_files/fuel_use_region.R
+++ wrangle_files/fuel_use_region.R
@@ -5,14 +5,28 @@ library(here)
 fuel_use <- read_csv(here("data","by_fuel_by_end_use.csv"))
 
 fuel_use_tidy <- fuel_use %>%
-  pivot_longer(cols=c(4:7), names_to = "fuel", values_to = "btu") 
+  pivot_longer(cols=c(4:7), names_to = "fuel", values_to = "btu") %>%
+  mutate(MJ = btu*0.011*1000) ##this goes from trillion BTU to billion MJ
 
+fuel_use_tidy_test <-   fuel_use_tidy %>%
+  filter(end_use %in% "Space Heating", census_region %in% "Midwest") %>%
+  group_by(sub_region) %>%
+  summarize(MJ = sum(MJ, na.rm=TRUE))
 
 fuel_use_tot <- fuel_use_tidy %>%
-  group_by(census_region,end_use,sub_region,fuel) %>%
-  summarize(btu=sum(btu, na.rm=TRUE))
+  group_by(census_region,fuel,end_use) %>%
+  summarize(MJ=sum(MJ, na.rm=TRUE))
+
+
+
+
+fuel_use_tot_test <- fuel_use_tot %>%
+  filter(census_region %in% "Northeast")%>%
+  filter(end_use %in% "Space Heating") %>%
+  # group_by(sub_region, fuel)%>%
+  summarize(sum=sum(MJ))
 
 
 fuel_use_coll_all <- fuel_use_tidy %>%
   group_by(census_region, sub_region, end_use) %>%
-  summarize(btu=sum(btu, na.rm=TRUE)) 
+  summarize(MJ=sum(MJ, na.rm=TRUE)) 
